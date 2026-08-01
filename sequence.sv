c6a931cbffc52965b03bdfeb693d86eb9779e5d8
Manan-Pambelu <mananmohandasp@gmail.com>
2026-08-01 12:08:41 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -30,7 +30,7 @@ class seq_with_res extends uvm_sequence #(trans);
         req=trans::type_id::create("req");
         begin
                    start_item(req);
-                        assert(req.randomize() with req.res==1;);
+						assert(req.randomize() with {req.res==1});
                    finish_item(req);
         end
 
@@ -50,7 +50,7 @@ class seq_without_ce extends uvm_sequence #(trans);
         req=trans::type_id::create("req");
         begin
                    start_item(req);
-                        assert(req.randomize() with req.res==0; req.ce=0;);
+						assert(req.randomize() with {req.res==0; req.ce=0});
                    finish_item(req);
         end
 
@@ -70,7 +70,7 @@ class seq_with_arithmatic extends uvm_sequence #(trans);
         req=trans::type_id::create("req");
         begin
                    start_item(req);
-                        assert(req.randomize() with req.res==0; req.ce==1; req.mode==1; req.cmd inside {[0:10]};);
+			           assert(req.randomize() with {req.res==0; req.ce==1; req.mode==1; req.cmd inside {[0:10]};});
                    finish_item(req);
         end
 
@@ -91,7 +91,7 @@ class seq_with_logical extends uvm_sequence #(trans);
         req=trans::type_id::create("req");
         begin
                    start_item(req);
-                        assert(req.randomize() with req.res==0; req.ce==1; req.mode==0; req.cmd inside {[0:13]};);
+						assert(req.randomize() with {req.res==0; req.ce==1; req.mode==0; req.cmd inside {[0:13]};});
                    finish_item(req);
         end
 
