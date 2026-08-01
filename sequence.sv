4f41edd462017ca4c3aeccd735d4b0d2407ab1eb
Manan-Pambelu <mananmohandasp@gmail.com>
2026-08-01 13:13:58 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -97,3 +97,59 @@ class seq_with_logical extends uvm_sequence #(trans);
 
  endtask
 endclass
+
+//------------------------------------------------------------------------------------------------------------
+
+class seq_timeout_test extends uvm_sequence #(trans);
+    `uvm_object_utils(seq_timeout_test)
+
+    function new(string name="seq_timeout_test");
+        super.new(name);
+    endfunction
+
+    task body();
+        req = trans::type_id::create("req");
+		
+        start_item(req);
+        assert(req.randomize() with { inp_valid == 2'b01; ce == 1; });
+        finish_item(req);
+
+        for(int i = 0; i < 17; i++) begin
+            start_item(req);
+            assert(req.randomize() with { inp_valid == 2'b00; ce == 1; });
+            finish_item(req);
+        end
+
+        start_item(req);
+        assert(req.randomize() with { inp_valid == 2'b10; ce == 1; });
+        finish_item(req);
+    endtask
+endclass
+
+//------------------------------------------------------------------------------------------------------
+
+class seq_delayed_valid extends uvm_sequence #(trans);
+    `uvm_object_utils(seq_timeout_test)
+
+	function new(string name="seq_delayed_valid");
+        super.new(name);
+    endfunction
+
+    task body();
+        req = trans::type_id::create("req");
+
+        start_item(req);
+        assert(req.randomize() with { inp_valid == 2'b01; ce == 1; });
+        finish_item(req);
+
+		for(int i = 0; i < $urandom_range(0,15); i++) begin
+            start_item(req);
+            assert(req.randomize() with { inp_valid == 2'b00; ce == 1; });
+            finish_item(req);
+        end
+
+        start_item(req);
+        assert(req.randomize() with { inp_valid == 2'b10; ce == 1; });
+        finish_item(req);
+    endtask
+endclass
